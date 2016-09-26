deabf4f0a0c23a975339effacefe71d6f0f7798a
Andrey Radantsev <avradantsev@yandex.ru>
2016-09-26 22:31:44 +0300

    тестовое задание

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,12 @@
 
 $(document).ready(function(){
-  // $(function(){
-  //   $('.bgc').css('height', 0);
-  // }());
-
   $(function(){
     $('.item__foot-checkbox').on('click', function(){
 
     var $this = $(this),
-        item = $this.closest('.courses__item'),
-        h = item.innerHeight();
-
-
-
-
-    // bgc.css('height', h);
-    console.log(h);
-
+        item = $this.closest('.courses__item');
 
     item.delay(1000).fadeOut();
-    // bgc.css('height', 0);
     });
   }());
-
-  // $(function(){
-  //   var a = $('.courses__item').innerHeight();
-  //
-  //       console.log(a);
-  // });
-
 });
